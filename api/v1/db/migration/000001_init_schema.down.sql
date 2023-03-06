8960a895c85550b1b4bec17d05bd4617d555186d
Ishan Shrestha <ishan.shrestha356@gmail.com>
2023-03-06 11:30:32 +0545

    feat: add changes to sql create script

---
+++ api/v1/db/migration/000001_init_schema.down.sql
@@ -0,0 +1,5 @@
+DROP TABLE "tag_post";
+DROP TABLE "comments";
+DROP TABLE "tags";
+DROP TABLE "posts";
+DROP TABLE "users";
--- api/v1/db/migration/000001_init_schema.up.sql
+++ api/v1/db/migration/000001_init_schema.up.sql
@@ -19,7 +19,7 @@ CREATE TABLE "posts" (
     created_at TIMESTAMPTZ,
     updated_at TIMESTAMPTZ,
     user_id INTEGER,
-    FOREIGN KEY (user_id) REFERENCES users (id)
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
 CREATE TABLE "tags" (
@@ -33,16 +33,18 @@ CREATE TABLE "tag_post" (
     post_id INTEGER,
     tag_id INTEGER,
     PRIMARY KEY (post_id, tag_id),
-    CONSTRAINT fk_post FOREIGN KEY(post_id) REFERENCES posts(id),
-    CONSTRAINT fk_tag FOREIGN KEY(tag_id) REFERENCES tags(id)
+    CONSTRAINT fk_post FOREIGN KEY(post_id) REFERENCES posts(id) ON DELETE CASCADE,
+    CONSTRAINT fk_tag FOREIGN KEY(tag_id) REFERENCES tags(id) ON DELETE CASCADE
 );
 
 CREATE TABLE "comments" (
-    id INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     content VARCHAR(1000) NOT NULL,
     rating INTEGER NOT NULL CHECK (rating >0 AND rating < 6),
     created_at TIMESTAMPTZ,
     updated_at TIMESTAMPTZ,
+    post_id INTEGER,
     user_id INTEGER,
-    CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES users(id)
+    CONSTRAINT fk_post FOREIGN KEY(post_id) REFERENCES posts(id) ON DELETE CASCADE,
+    CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
 );
